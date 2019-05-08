2d4bf6cbb9e17653e337d2888cb59c2d63b12ede
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-05-08 00:16:54 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./jgrace/precache-manifest.f075e413ce6d6cd907293e0fc061be2b.js"
+  "./precache-manifest.879448e00b81c101b1c5bf94a83554f2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./jgrace/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
