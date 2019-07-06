8b25f447900fa94e7efae014935a58794be2888d
Daniel KwanUk Yoo <kwanukyoo0722@gmail.com>
2019-07-06 13:34:58 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.413828015b6395fbd1e966e0a97d2cf1.js"
+  "/precache-manifest.8d640069c2c256b1469f65de26dd9bbd.js"
 );
 
 workbox.clientsClaim();
