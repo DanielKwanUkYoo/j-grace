eff0055b8bdf63cb3f568aaec230edc178282375
J Grace <jgrace@Js-MacBook-Pro.local>
2020-12-19 23:24:53 -0800

    Updates

--- precache-manifest.37e059d115639096b0b40321f997bed5.js
+++ precache-manifest.37e059d115639096b0b40321f997bed5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "0b2ec6940db50e68e904",
-    "url": "/static/css/main.4ade81ab.chunk.css"
+    "revision": "ed8dd74605b696fa6802",
+    "url": "/static/css/main.0b1561a1.chunk.css"
   },
   {
-    "revision": "0b2ec6940db50e68e904",
-    "url": "/static/js/main.85733c5f.chunk.js"
+    "revision": "ed8dd74605b696fa6802",
+    "url": "/static/js/main.42ebff5d.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "ff32468757588f6865c0",
+    "revision": "5ccc2afbef5d97429f82",
     "url": "/static/css/2.ee501967.chunk.css"
   },
   {
-    "revision": "ff32468757588f6865c0",
-    "url": "/static/js/2.fe53a32d.chunk.js"
+    "revision": "5ccc2afbef5d97429f82",
+    "url": "/static/js/2.5f3866ec.chunk.js"
   },
   {
     "revision": "eca7614c8433284fdb0b2bd1d4193fb9",
@@ -44,12 +44,12 @@ self.__precacheManifest = [
     "url": "/static/media/lastWeeklyUpdate.71e7dda6.png"
   },
   {
-    "revision": "65e9504a8bec33235cd5b272df8369d9",
-    "url": "/static/media/carouselTwo.65e9504a.jpg"
-  },
-  {
     "revision": "96284b46b5719a864bbf97ec3872ceb2",
     "url": "/static/media/carouselOne.96284b46.jpg"
+  },
+  {
+    "revision": "b5548b50bb177572857a7e57ab797c36",
+    "url": "/static/media/carouselTwo.b5548b50.jpg"
   },
   {
     "revision": "243c09b6efb7c6b1e92363e1fd108a1f",
@@ -60,12 +60,12 @@ self.__precacheManifest = [
     "url": "/static/media/2020Mission.682f2eb7.pdf"
   },
   {
-    "revision": "4f246eacdfb032a1bb4bc5852f27ad2b",
-    "url": "/static/media/thisMonth.4f246eac.png"
-  },
-  {
     "revision": "59f4e2b534382725acb2864baac5d6e7",
     "url": "/static/media/service.59f4e2b5.png"
+  },
+  {
+    "revision": "4f246eacdfb032a1bb4bc5852f27ad2b",
+    "url": "/static/media/thisMonth.4f246eac.png"
   },
   {
     "revision": "3e425fd21f75ffa63ee40fd01748098a",
@@ -360,6 +360,10 @@ self.__precacheManifest = [
     "url": "/static/media/20201213.628cea11.png"
   },
   {
+    "revision": "ac093424da25ec08da88b40925dab110",
+    "url": "/static/media/20201220.ac093424.png"
+  },
+  {
     "revision": "3163a84f562cfca121badb6930cc0a7a",
     "url": "/static/media/covid19.3163a84f.jpg"
   },
@@ -376,15 +380,15 @@ self.__precacheManifest = [
     "url": "/static/media/slick.b7c9e1e4.woff"
   },
   {
-    "revision": "f97e3bbf73254b0112091d0192f17aec",
-    "url": "/static/media/slick.f97e3bbf.svg"
-  },
-  {
     "revision": "d41f55a78e6f49a5512878df1737e58a",
     "url": "/static/media/slick.d41f55a7.ttf"
   },
   {
-    "revision": "f96cd372b1c76d4a3bc6e70f6786451c",
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "57c825cba118df2c75298ef91fd04fda",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a1ddf80b55113c380def7e0eccd79f3.js"
+  "/precache-manifest.37e059d115639096b0b40321f997bed5.js"
 );
 
 workbox.clientsClaim();
